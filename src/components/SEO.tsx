d7cf608fb8ce4b9be2235a70e52c46a2128b6b28
amosoni <13860392700@163.com>
2025-08-05 19:35:58 +0800

    fix(favicon): 添加SVG favicon解决图标缺失问题

--- src/components/SEO.tsx
+++ src/components/SEO.tsx
@@ -31,6 +31,7 @@ const SEO: React.FC<SEOProps> = ({
       <link rel="canonical" href={canonicalUrl} />
       
       {/* Favicon */}
+      <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
       <link rel="icon" href="/favicon.ico" />
       <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
       
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -13,6 +13,7 @@ export default function Document() {
         <link rel="canonical" href="https://soniceai.com/" />
         
         {/* Favicon */}
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
         <link rel="icon" href="/favicon.ico" />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
         
